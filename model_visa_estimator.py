4ebbc62e5e7c0dfbefcb27fa9f460f8cdb52a5b1
Prarthana-1910 <prarthanapatil2006@gmail.com>
2025-12-11 18:11:31 +0530

    Adding the copy of missing values dataset

--- model_visa_estimator.py
+++ model_visa_estimator.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
 
-#reading csv file with real time dates
-df=pd.read_csv("visa_dataset.csv", parse_dates=['Application_Date','Decision_Date']) 
-print(df.isnull().sum())
+#reading csv file having missing values with real time dates
+df1=pd.read_csv("visa_dataset_final_missing.csv", parse_dates=['Application_Date','Decision_Date']) 
+print(df1.isnull().sum())
+
+#reading preprocessed csv file 
+df=pd.read_csv("visa_dataset.csv",parse_dates=['Application_Date','Decision_Date'])
 
 #handling missing values
 df['Decision_Date'].fillna(df['Decision_Date'].mode()[0], inplace=True)
@@ -13,6 +16,9 @@ df['Gender'].fillna('Unknown',inplace=True)
 df['Applicant_Age'].fillna(round(df['Applicant_Age'].median()),inplace=True)
 df['Processing_Center'].fillna('Unknown',inplace=True)
 df['Case_Status'].fillna('Unknown',inplace=True)
+
+#checking whether the missing values are handled or not
+print(df.isnull().sum())
 
 #adding application month as an attribute
 df['Application_Month']=df['Application_Date'].dt.month_name()
