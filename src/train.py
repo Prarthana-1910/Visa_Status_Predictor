17310c21bf41a64fefbae0f8f00e8e59370a3c9c
Prarthana-1910 <prarthanapatil2006@gmail.com>
2026-01-13 19:54:35 +0530

    Created Preprocessing pipeline

--- src/train.py
+++ src/train.py
@@ -1,48 +1,70 @@
 import pandas as pd
-import numpy as np
 import joblib
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor
 from sklearn.impute import SimpleImputer
-
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 
 df=pd.read_csv(r"data\features.csv")
-df_encoded=pd.get_dummies(df,columns=["Peak_Season","Delay_Status"])
 
-X=df_encoded.drop(columns=["Processing_Days"])
-y=df_encoded["Processing_Days"]
+#Defining features and target
+cat_features=["Peak_Season","Delay_Status"]
+num_features=[col for col in df.columns if col not in cat_features+["Processing_Days"]]
+
+X=df[cat_features+num_features]
+y=df["Processing_Days"]
 X_train,X_test,y_train,y_test=train_test_split(
     X,y,test_size=0.2,random_state=42
 )
 
-imputer=SimpleImputer(strategy='mean')
-X_train=imputer.fit_transform(X_train)
-X_test=imputer.transform(X_test)
+#Preprocessing
+preprocessor=ColumnTransformer(
+    transformers=[
+        ("OneHot",OneHotEncoder(handle_unknown="ignore"),cat_features),
+        ("num",SimpleImputer(strategy="mean"),num_features)
+    ]
+)
 
+#Define models with Pipelines
 models={
-    "Linear Regression": LinearRegression(),
-    "Random Forest Regressor": RandomForestRegressor(random_state=42),
-    "Gradient Boosting regressor": GradientBoostingRegressor(random_state=42)
+    "Linear Regression": Pipeline([
+        ("preprocessor",preprocessor),
+        ("model",LinearRegression())
+    ]),
+    "Random Forest Regressor": Pipeline([
+        ("preprocesor",preprocessor),
+        ("model",RandomForestRegressor(random_state=42))
+    ]),
+    "Gradient Boosting regressor": Pipeline([
+        ("preprocessor",preprocessor),
+        ("model",GradientBoostingRegressor(random_state=42))
+    ])
 }
-results={}
-for name,model in models.items():
-    model.fit(X_train,y_train)
-    joblib.dump(model,f"models/{name}.joblib")
 
-joblib.dump(imputer,"models/imputer.joblib")
+for name,model_pipeline in models.items():
+    model_pipeline.fit(X_train,y_train)
+    joblib.dump(model_pipeline,f"models/{name.replace(' ','_')}_pipeline.joblib")
+    print(f"{name} trained and saved!")
 
 #Hyperparameter tuning
 param_grid={
-    "n_estimators":[100, 200, 300],
-    "learning_rate":[0.05, 0.1, 0.2],
-    "max_depth":[3, 4, 5],
-    "subsample":[0.8, 1.0],
-    "min_samples_leaf":[1, 3, 5]
+    "model__n_estimators":[100, 200, 300],
+    "model__learning_rate":[0.05, 0.1, 0.2],
+    "model__max_depth":[3, 4, 5],
+    "model__subsample":[0.8, 1.0],
+    "model__min_samples_leaf":[1, 3, 5]
 }
-gbr=GradientBoostingRegressor(random_state=42)
+
+gbr_pipeline=Pipeline([
+    ("preprocessor",preprocessor),
+    ("model",GradientBoostingRegressor(random_state=42))
+])
+
 grid_search=GridSearchCV(
-    estimator=gbr,
+    estimator=gbr_pipeline,
     param_grid=param_grid,
     scoring="neg_mean_absolute_error",
     cv=5,
@@ -50,8 +72,9 @@ grid_search=GridSearchCV(
     verbose=1
 )
 grid_search.fit(X_train,y_train)
-best_gbr=grid_search.best_estimator_
-joblib.dump(best_gbr,"models/gradient_boost_tuned.joblib")
+
+best_gbr_pipeline=grid_search.best_estimator_
+joblib.dump(best_gbr_pipeline,"models/gradient_boost_pipeline_tuned.joblib")
 joblib.dump(grid_search.best_params_,"models/gradient_boost_params.joblib")
 print("Best hyperparameters found:")
 print(grid_search.best_params_)
